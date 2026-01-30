01c5eec9fa6dac82b6cbd3dbce2fbbd65d91512e
GameGramPlay <aeahb003805mg@ahbs.ae>
2026-01-30 10:41:35 +0400

    Update Message.jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,89 +1,93 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Flex, Text, Image, Icon } from "@chakra-ui/react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-dayjs.extend(relativeTime);
 import { MdVerified } from "react-icons/md";
 import { truncateText } from "../utils";
 
-export default function Message({ message, isYou }) {
+dayjs.extend(relativeTime);
+
+export default function Message({ message, isYou, showUsername = true }) {
   const countyCode =
     message.country && message.country !== "undefined"
       ? message.country.toLowerCase()
       : "";
+
+  const bubbleBg = isYou ? "#3ba55d" : "#36393f"; // Discord-like green for you, dark gray for others
+  const textColor = isYou ? "white" : "gray.200";
+
   return (
-    <Box display="grid" justifyItems={isYou ? "end" : "start"}>
-      <Grid
-        templateRows="30px 1fr 25px"
-        templateColumns="1fr"
-        w="70%"
+    <Flex
+      mb="2"
+      justify={isYou ? "flex-end" : "flex-start"}
+      align="flex-start"
+      w="100%"
+      px="2"
+    >
+      <Box
+        position="relative"
+        maxW="70%"
+        bg={bubbleBg}
+        color={textColor}
         px="3"
         py="2"
-        borderRadius="5px"
-        borderTopLeftRadius={isYou ? "5px" : "0"}
-        borderTopRightRadius={isYou ? "0" : "5px"}
-        bg={isYou ? "#dbfff9" : "#edf3f9"}
-        mt="5"
-        position="relative"
-        _after={{
-          position: "absolute",
-          content: "''",
-          width: 0,
-          height: 0,
-          borderStyle: "solid",
-          borderWidth: isYou ? "0px 0px 10px 10px" : "0px 10px 10px 0",
-          borderColor: isYou
-            ? "transparent transparent transparent #dbfff9"
-            : "transparent #edf3f9 transparent transparent",
-          top: 0,
-          left: isYou ? "auto" : "-10px",
-          right: isYou ? "-10px" : "auto",
-        }}
+        borderRadius="16px"
+        borderTopLeftRadius={isYou ? "16px" : "4px"}
+        borderTopRightRadius={isYou ? "4px" : "16px"}
+        boxShadow="sm"
+        _hover={{ filter: "brightness(1.05)" }}
       >
-        <GridItem
-          fontWeight="500"
-          fontSize="md"
-          justifySelf="start"
-          color="gray.500"
-          mb="2"
-        >
-          <span>{message.username} </span>
-          {message.is_authenticated && (
-            <MdVerified
-              color="#1d9bf0"
-              style={{ display: "inline", marginRight: "5px" }}
-            />
-          )}
-          {countyCode && (
-            <Box display="inline-block" fontSize="10px">
-              from {message.country}{" "}
-              <img
-                style={{ display: "inline-block", marginTop: "-4px" }}
-                src={`/flags/${countyCode}.png`}
-                alt={message.country}
-              />
-            </Box>
-          )}
-        </GridItem>
-        <GridItem
-          justifySelf="start"
-          textAlign="left"
-          wordBreak="break-word"
-          fontSize="md"
-          fontFamily="Montserrat, sans-serif"
-          color="gray.800"
-        >
-          {truncateText(message.text)}
-        </GridItem>
+        {/* Pointer triangle */}
+        <Box
+          position="absolute"
+          top="0"
+          left={isYou ? "auto" : "-8px"}
+          right={isYou ? "-8px" : "auto"}
+          width={0}
+          height={0}
+          borderStyle="solid"
+          borderWidth={isYou ? "0 0 10px 10px" : "0 10px 10px 0"}
+          borderColor={
+            isYou
+              ? `transparent transparent transparent ${bubbleBg}`
+              : `transparent ${bubbleBg} transparent transparent`
+          }
+        />
 
-        <GridItem
-          color="gray"
-          fontSize="10px"
-          justifySelf="end"
-          alignSelf="end"
-        >
+        {/* Username + verified + country */}
+        {showUsername && (
+          <Flex align="center" mb="1">
+            <Text fontWeight="600" fontSize="sm" mr="1">
+              {message.username}
+            </Text>
+            {message.is_authenticated && (
+              <Icon as={MdVerified} color="#1d9bf0" w={4} h={4} mr="1" />
+            )}
+            {countyCode && (
+              <Flex align="center" fontSize="xs" color={isYou ? "gray.200" : "gray.400"}>
+                from {message.country}
+                <Image
+                  src={`/flags/${countyCode}.png`}
+                  alt={message.country}
+                  w="16px"
+                  h="11px"
+                  ml="1"
+                  mt="-1px"
+                />
+              </Flex>
+            )}
+          </Flex>
+        )}
+
+        {/* Message text */}
+        <Text fontSize="md" whiteSpace="pre-wrap" wordBreak="break-word">
+          {truncateText(message.text)}
+        </Text>
+
+        {/* Timestamp */}
+        <Text fontSize="xs" color={isYou ? "gray.200" : "gray.400"} mt="1" textAlign="right">
           {dayjs(message.timestamp).fromNow()}
-        </GridItem>
-      </Grid>
-    </Box>
+        </Text>
+      </Box>
+    </Flex>
   );
 }
