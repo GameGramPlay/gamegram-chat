1e72d340685a68063bb998159f41bf6bc18d886a
GameGramPlay <aeahb003805mg@ahbs.ae>
2026-01-30 18:40:29 +0400

    Update Footer.jsx

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,37 +1,62 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Flex, Link, Text, Icon } from "@chakra-ui/react";
 import { FaGithub, FaTwitter } from "react-icons/fa";
 import MessageForm from "../components/MessageForm";
+
 export default function Footer() {
   return (
-    <Box position="fixed" bottom="0" width="100%">
+    <Box
+      position="fixed"
+      bottom="0"
+      width="100%"
+      bg="#2f3136"      // Discord dark footer
+      borderTop="1px solid #202225"
+      zIndex={20}
+    >
+      {/* Message input */}
       <MessageForm />
 
+      {/* Footer links */}
       <Grid
-        gridTemplateColumns="auto 1fr"
-        textAlign="center"
+        gridTemplateColumns="auto 1fr auto"
         alignItems="center"
-        py="4px"
-        px="30px"
-        height="40px"
-        bg="white"
+        py="2"
+        px="4"
+        fontSize="sm"
+        color="gray.400"
       >
-        <GridItem justifySelf="start">
-          <a
+        {/* Left: GitHub link */}
+        <GridItem>
+          <Link
             href="https://gamegramplay.github.io/GameGram-website/"
-            target="_blank"
-            rel="noreferrer"
+            isExternal
+            display="flex"
+            alignItems="center"
+            _hover={{ color: "white" }}
           >
-            <FaGithub style={{ display: "inline" }} /> GameGram
-          </a>
+            <Icon as={FaGithub} mr="1" />
+            GameGram
+          </Link>
         </GridItem>
+
+        {/* Center: copyright / status */}
+        <GridItem justifySelf="center">
+          <Text fontSize="xs" color="gray.500">
+            &copy; {new Date().getFullYear()} GameGram. All rights reserved.
+          </Text>
+        </GridItem>
+
+        {/* Right: Twitter link */}
         <GridItem justifySelf="end">
-          <a
-            href="https://gamegramplay.github.io/GameGram-website/"
-            target="_blank"
-            rel="noreferrer"
+          <Link
+            href="https://twitter.com/gamegramplay"
+            isExternal
+            display="flex"
+            alignItems="center"
+            _hover={{ color: "#1d9bf0" }}
           >
-            <FaGithub style={{ display: "inline" }} /> GameGram
-          </a>
+            <Icon as={FaTwitter} mr="1" />
+            Twitter
+          </Link>
         </GridItem>
       </Grid>
     </Box>
