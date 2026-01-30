b0e1384cabe697325d916d55912a55b51affb719
GameGramPlay <aeahb003805mg@ahbs.ae>
2026-01-30 18:11:25 +0400

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,41 +1,68 @@
-import { Alert, Box, Button, Spinner } from "@chakra-ui/react";
+import { Alert, Box, Button, Spinner, VStack } from "@chakra-ui/react";
 import { useAppContext } from "../context/appContext";
 import Message from "./Message";
 
 export default function Messages() {
   const { username, loadingInitial, error, getMessagesAndSubscribe, messages } =
     useAppContext();
+
   const reversed = [...messages].reverse();
+
   if (loadingInitial)
     return (
-      <Box textAlign="center">
-        <Spinner />
+      <Box textAlign="center" mt="20px">
+        <Spinner size="xl" color="teal.400" />
       </Box>
     );
+
   if (error)
     return (
-      <Alert.Root status="error" mt="20px">
-        {error}
-        <Button
-          ml="5px"
-          onClick={getMessagesAndSubscribe}
-          colorScheme="red"
-          variant="link"
-        >
-          try to reconnect
-        </Button>
-      </Alert.Root>
+      <Alert status="error" mt="20px" borderRadius="md">
+        <Box>
+          {error}
+          <Button
+            ml="5px"
+            onClick={getMessagesAndSubscribe}
+            colorScheme="red"
+            variant="link"
+          >
+            try to reconnect
+          </Button>
+        </Box>
+      </Alert>
     );
 
   if (!messages.length)
     return (
-      <Box as="h3" textAlign="center">
+      <Box as="h3" textAlign="center" mt="20px" color="gray.400">
         No messages 😞
       </Box>
     );
 
-  return reversed.map((message) => {
-    const isYou = message.username === username;
-    return <Message key={message.id} message={message} isYou={isYou} />;
-  });
+  return (
+    <VStack
+      spacing={2}
+      align="stretch"
+      bg="gray.900"
+      p={4}
+      borderRadius="md"
+      h="calc(100vh - 100px)"
+      overflowY="auto"
+    >
+      {reversed.map((message) => {
+        const isYou = message.username === username;
+        return (
+          <Message
+            key={message.id}
+            message={message}
+            isYou={isYou}
+            style={{
+              alignSelf: isYou ? "flex-end" : "flex-start",
+              maxWidth: "70%",
+            }}
+          />
+        );
+      })}
+    </VStack>
+  );
 }
